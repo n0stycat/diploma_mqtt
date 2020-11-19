8bb24b2f395356e3391f2cacf0f90595a0bb3f67
n0sty <n0stycat@gmail.com>
2020-11-19 06:14:28 +0200

    Fixed wrong date, tested 'packetrecieve', renders right data, added navbar, styles, static folder

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const mqtt = require('mqtt');
+const path = require('path');
 const Broker_URL = 'mqtt://192.168.1.31';
 const options = {
     //clientId: 'clientId_example',
@@ -10,29 +11,30 @@ const options = {
 };
 
 const Message = require('../diploma_mqtt/models/messageSchema');
+const key = require('./config/key')
 
 const app = express();
 const port = 3000;
 app.engine('handlebars', hbs());
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, '/public')));
 
-const mongoURI = 'mongodb+srv://n0sty:mqtt@cluster0.z8myq.mongodb.net/mqtt_test_db?retryWrites=true&w=majority';
-mongoose.connect(mongoURI, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect(key.mongoURI, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
 
 const connection = mongoose.connection;
 connection.once('open', function () {
-    console.log('Connected to db');
+    console.log('Connected to MongoDB.')
     let client = mqtt.connect(Broker_URL, options);
 
     client.on('connect', function () {
         client.subscribe('#', function (err) {
             if (!err) {
                 console.log('Connected to MQTT broker.');
-                client.publish('status/', 'Successfully connected.');
+                //client.publish('status/', 'Successfully connected.');
             } else {
                 console.log('Error connecting to MQTT broker!');
             }
-        })
+        });
     });
 
     client.on('reconnect', function () {
@@ -44,12 +46,13 @@ connection.once('open', function () {
     });
 
     client.on('message', function (topic, message, packet) {
-        console.log(packet);
+        //console.log(packet);
         const msg = new Message({
             messageId: 'messageId_example',
             clientId: options.clientId,
             topic: packet.topic,
-            message: message
+            message: message,
+            dateTime: Date.now()
         });
         msg.save().then(() => {
             console.log('Message was saved to db.');
@@ -57,11 +60,20 @@ connection.once('open', function () {
             console.log('There was an error when saving \n' + msg + ' \nto db.');
         });
     });
+    client.on('packetreceive', function (packet) {
+        //console.log(packet);
+    })
 });
 
 app.get('/', async (req, res) => {
-    const msg = await Message.find({}).lean();
+    const msg = await Message.find({}, null, {sort: {dateTime: -1}}, function (err, result) {
+        if (!result) {
+            console.log('No messages in db.');
+        } else {
+        }
+    }).lean();
     res.render("home", {msg});
+
 
 });
 
