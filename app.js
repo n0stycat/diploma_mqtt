70cd062b950f234438cd5037073cd2e72fdfb3ce
n0sty <n0stycat@gmail.com>
2020-12-10 17:57:22 +0200

    Changed mosquitto to aedes, added TLS

--- app.js
+++ app.js
@@ -1,89 +1,94 @@
-const express = require('express');
-const hbs = require('express-handlebars');
-const mongoose = require('mongoose');
-const mqtt = require('mqtt');
+const mqemitter = require('mqemitter-mongodb');
+const aedesPersistenceMongoDB = require('aedes-persistence-mongodb');
+const fs = require('fs');
 const path = require('path');
-const morgan = require('morgan');
 
-const Message = require('../diploma_mqtt/models/messageSchema');
-const key = require('./config/key')
-
-const Broker_URL = 'mqtt://192.168.1.31';
-const options = {
-    //clientId: 'clientId_example',
-    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-    username: 'oladik',
-    password: 'topsecret',
-    port: 1883,
-    keepalive: 60
+const port = 8883; // TLS
+// const port = 1883; // TCP
+const MONGO_URL = 'mongodb+srv://n0sty:mqtt@cluster0.z8myq.mongodb.net/test_db';
+const optionsBroker = {
+    id: 'Aedes',
+    mq: mqemitter({
+        url: MONGO_URL
+    }),
+    persistence: aedesPersistenceMongoDB({
+        url: MONGO_URL,
+    })
+};
+const optionsServer = {
+    key: fs.readFileSync(path.join('./config') + path.join('/broker') + path.join('/broker.key')),
+    cert: fs.readFileSync(path.join('./config') + path.join('/broker') + path.join('/broker.crt')),
 };
 
-const app = express();
-const port = 3000;
-app.engine('handlebars', hbs());
-app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(morgan('combined', ''));
+function startAedes() {
+    const aedes = require('aedes')(optionsBroker);
 
-mongoose.connect(key.mongoURI, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-const connection = mongoose.connection;
-
-connection.once('open', function () {
-    console.log('Connected to MongoDB.');
-    let client = mqtt.connect(Broker_URL, options);
-
-    client.on('connect', function () {
-        client.subscribe('#', function (err) {
-            if (!err) {
-                console.log('Connected to MQTT broker.');
-                //client.publish('status/', 'Successfully connected.');
-            } else {
-                console.log('Error connecting to MQTT broker!');
-            }
-        });
-    });
-
-    client.on('reconnect', function () {
-        console.log('Reconnecting...');
-        client = mqtt.connect(Broker_URL, options);
-    });
-
-    client.on('error', function (err) {
-        console.log(err);
-    });
-
-    client.on('message', function (topic, message, packet) {
-        //console.log(packet);
-        const msg = new Message({
-            clientId: options.clientId,
-            topic: packet.topic,
-            message: message,
-            dateTime: Date.now()
-        });
-        msg.save().then(() => {
-            console.log('Message was saved to db.');
-        }).catch(() => {
-            console.log('There was an error when saving \n' + msg + ' \nto db.');
-        });
-    });
-    client.on('packetreceive', function (packet) {
-        //console.log(packet);
-    });
-    client.on('offline', function () {
-        console.log('MQTT broker is currently offline.');
-    });
-});
-
-app.get('/', async (req, res) => {
-    const msg = await Message.find({}, null, {sort: {dateTime: -1}}, function (err, result) {
-        if (!result) {
-            console.log('No messages in db.');
-        } else {
+    // aedes.authorizeSubscribe = function(client, sub, callback) {
+    //          let err = null;
+    //          if (sub.topic === 'forbidden_sub') {
+    //              sub = null;
+    //              err = Error('Not authorized');
+    //          }
+    //          callback(err, sub);
+    //      }
+    aedes.authorizeSubscribe = function (client, sub, callback) {
+        if (sub.topic === 'forbidden_sub') {
+            return callback(new Error('wrong topic'));
         }
-    }).lean();
-    res.render("home", {msg});
-});
+        callback(null, sub);
+    };
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
+    aedes.authorizePublish = function (client, packet, callback) {
+        if (packet.topic === 'forbidden_pub') {
+            return callback(new Error('wrong topic'));
+        }
+        if (packet.topic === 'forbidden_pub2') {
+            packet.payload = Buffer.from('overwrite packet payload');
+        }
+        callback(null);
+    };
+
+    //   aedes.authenticate = function (client, username, password, callback) {
+    //     callback(null, (username === 'oladik'));
+    // }
+    const server = require('tls').createServer(optionsServer, aedes.handle); // TLS
+    // const server = require('net').createServer(aedes.handle); // TCP
+
+    server.listen(port, function () {
+        console.log('Aedes listening on port:', port);
+        //aedes.publish({topic: 'aedes/hello', payload: "I'm broker " + aedes.id});
+    });
+
+    aedes.on('subscribe', function (subscriptions, client) {
+        console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
+            '\x1b[0m subscribed to topics: ' + subscriptions.map(s => s.topic).join('\n'), 'from broker', aedes.id);
+    })
+
+    aedes.on('unsubscribe', function (subscriptions, client) {
+        console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
+            '\x1b[0m unsubscribed to topics: ' + subscriptions.join('\n'), 'from broker', aedes.id);
+    })
+
+    aedes.on('client', function (client) {
+        console.log('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id);
+    })
+
+    aedes.on('clientError', function (err) {
+        console.log('error');
+        console.log(err);
+    })
+
+    aedes.on('clientDisconnect', function (client) {
+        console.log('Client Disconnected: \x1b[31m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id);
+    })
+
+    aedes.on('publish', async function (packet, client) {
+        console.log('Client \x1b[31m' + (client ? client.id : 'BROKER_' + aedes.id) + '\x1b[0m has published', packet.payload.toString(), 'on', packet.topic)
+        // let clientName;
+        // client ? clientName = client.id.slice(0, 36) : clientName = client;
+        // aedes.mq.emit(packet);
+        // console.log(packet);
+    })
+}
+
+startAedes()
--- models/messageSchema.js
+++ models/messageSchema.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema
 const messageSchema = Schema({
     topic: {type: String, required: true},
     message: {type: String, required: true},
-    clientId: {type: String, required: true},
     dateTime: {type: Date, default: Date.now(), required: true}
 });
 
